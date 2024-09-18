fb549fabff0df1a825046c9cbc369a13b998d02c
Larysa Rud <larpavrud@gmail.com>
2024-09-18 10:01:45 -0400

    Rendering the game board

--- src/components/TicTacToe.jsx
+++ src/components/TicTacToe.jsx
@@ -35,6 +35,15 @@ const TicTacToe = () => {
       <br />
       <div className="status">{getStatus()}</div>
       <div className="board">
+        <div className="board-row">
+          {renderSquare(0)} {renderSquare(1)} {renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {renderSquare(3)} {renderSquare(4)} {renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {renderSquare(6)} {renderSquare(7)} {renderSquare(8)}
+        </div>
       </div>
     </div>
   );
