03a3008a5aba8f87750fbd3f60ee334a89ef0d71
Larysa Rud <larpavrud@gmail.com>
2024-09-18 09:46:53 -0400

    Setting up the initial structure of the Tic Tac Toe component, initializing the state for the game board and whose turn it is (X or O)

--- src/components/TicTacToe.jsx
+++ src/components/TicTacToe.jsx
@@ -1,12 +1,19 @@
+import { useState } from 'react';
+
 const TicTacToe = () => {
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [isXNext, setIsXNext] = useState(true);
+  const winner = calculateWinner(board);
 
+  return (
+    <div className="game">
+      {/* <h1>Tic-Tac-Toe</h1> */}
+      <br />
+      <div className="status">{getStatus()}</div>
+      <div className="board">
+      </div>
+    </div>
+  );
+};
 
-    return (
-           <div className="game">
-               <h1>Tic-Tac-Toe</h1>
-               
-           </div>
-       );
-   };
-   
-   export default TicTacToe;
+export default TicTacToe;
